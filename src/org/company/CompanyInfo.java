105081f3af80d619f4acb57f43a1785090309c69
VigneshS02121994 <vigneshitvijay@gmail.com>
2022-04-21 18:47:00 +0530

    company details

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,19 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyName() {
+		System.out.println("company name is Eastman Exports");
+	}
+	public void companyId() {
+		System.out.println("company id is 5026");
+	}
+	public void companyAddress() {
+		System.out.println("company address is Tiruppur-641603");
+	}
+	public static void main(String[]args) {
+		CompanyInfo l = new CompanyInfo();
+		l.companyName();
+		l.companyId();
+		l.companyAddress();
+		}
+	}
